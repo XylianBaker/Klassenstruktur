4738aecf7cc3384f1d308e4a31761a0cc1c27681
Jan Kammellander <janschwiderik@gmail.com>
2020-12-20 15:39:47 +0100

    trying out some Object-methods 🚇

--- person.js
+++ person.js
@@ -1,16 +1,28 @@
+// Cosntructure Syntax 🚧
+const Person = new Object();
+Person.surename = 'Jan';
+Person.lastname = 'Kammellander';
+Person.height = '174';
+Person.gender = '👨';
+Person.age = 16;
+
 // Object literal syntax 🤺
-const Person = {
+const teacher = {
     surename: 'Jan',
     lastname: 'Kammellander',
     height: 172,
     gender: '👨',
     age: '16'
-}
-
-// Cosntructure Syntax 🚧
-const teacher = new Object();
-teacher.subject = 'maths➗';
+};
+Person.subject = 'maths➗';
 
 // Static Method 🗿 --> best for inheriting an existing Object "Vererbung"
 // Using the Person 🤺 as an Prototyp
 const student = Object.create(Person);
+Object.defineProperty(student, 'subject', {
+    get: () => 'maths➗',
+});
+
+console.log(student.gender);
+// Original Object 🧭
+console.log(Object.getPrototypeOf(student));
